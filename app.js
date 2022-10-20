b7c4660dbb8ab9e413f7741e7f3d2e0a91d09841
Lucas Epherra <letoso13@gmail.com>
2022-10-20 20:57:03 -0300

    comenzando el desafio

---
+++ app.js
@@ -0,0 +1,27 @@
+/* COTIZADOR DE DIVISAS */
+// a continuacion se realizara un simulador interactivo de un conversor de divisas ( euro, pesoArg, dolar y sus variantes blue)
+
+let pesoArgentino = 1;
+let dolarOficial = 200;
+let dolarBlue = 300;
+let euroOficial = 210;
+let euroBlue = 310;
+
+function moneda(val1, val2, val3, val4, val5) {
+
+    let monedElegida = parseInt(prompt("Elija una moneda"));
+
+
+}
+
+moneda();
+
+
+function montoIngresado() {
+
+    let = prompt("Ingrese un monto");
+
+
+}
+
+montoIngresado();
