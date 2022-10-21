f30c825bab62856b80a2e13bd007eef6417d7c78
Lucas Epherra <letoso13@gmail.com>
2022-10-21 02:32:12 -0300

    comit a ghub para mi notebook

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /* COTIZADOR DE DIVISAS */
-// a continuacion se realizara un simulador interactivo de un conversor de divisas ( euro, pesoArg, dolar y sus variantes blue)
+// a continuacion se realizara un simulador interactivo de un conversor de divisas ( euro, pesoArg, dolar y sus variantes blue) con lo visto hasta la clase 4.
+
+//variables ( monedas y valor )
 
 let pesoArgentino = 1;
 let dolarOficial = 200;
@@ -7,19 +9,34 @@ let dolarBlue = 300;
 let euroOficial = 210;
 let euroBlue = 310;
 
-function moneda(val1, val2, val3, val4, val5) {
 
-    let monedElegida = parseInt(prompt("Elija una moneda"));
+function elegirMoneda(val1, val2, val3, val4, val5) {
 
+    let monedaElegida = prompt("Elija una moneda");
+
+    if (monedaElegida == "")  {
+
+        alert ("No elegiste un tipo de moneda valido");
+
+    } else {
+
+        prompt ("")
+
+        val1 = pesoArgentino;
+        val2 = dolarOficial;
+        val3 = dolarBlue;
+        val4 = euroOficial;
+        val5 = euroBlue;
+    }
 
 }
 
-moneda();
+elegirMoneda();
 
 
 function montoIngresado() {
 
-    let = prompt("Ingrese un monto");
+    let = parseInt(prompt("Ingrese un monto"));
 
 
 }
