fea24b7d7ea358815194cda3be62f3ce3b566cff
Lucas Epherra <letoso13@gmail.com>
2022-10-23 20:55:10 -0300

    proyecto v2

--- app.js
+++ app.js
@@ -1,61 +1,100 @@
-/* COTIZADOR DE DIVISAS */
-// a continuacion se realizara un simulador interactivo de un conversor de divisas ( euro, pesoArg, dolar y sus variantes blue) con lo visto hasta la clase 4.
+/* CALCULADORA DE AHORRO EN DIVISAS EXTRANJERAS*/
+
+/* a continuacion se realizara un simulador interactivo de un conversor de divisas ( euro, dolar y sus variantes blue) con lo visto hasta la clase 4. */
 
 //variables ( monedas y valor )
-const peso = 1;
+
 const dolar = 200;
 const dolarBlue = 300;
-const euro = 210;
-const euroBlue = 310;
+const euro = 250;
+const euroBlue = 350;
+
+let numeroInvalido;
+let divisa;
+
 
 // funcion para verificar que en ahorro se tipee solo numeros
-function funcionAhorro() {
+ function funcionAhorro() {
 
-    let ahorro = Number(prompt("Ingrese sus ahorros en pesos"));
+    let ahorro = (prompt("Ingrese sus ahorros en pesos"));
 
     if (ahorro !== "" || ahorro !== null || ahorro !== NaN) {
         return ahorro
     } else { return 404 }
 }
+ 
+numeroInvalido = 404;
 
-// prompt para que el usuario elija una moneda 
-let moneda = prompt("Elige una moneda entre (dolar,dolar blue,euro,euro blue)");
-
-// ciclo while para que solo se puedan elegir las 4 opciones
-while (moneda != "dolar" && moneda != "dolar blue" && moneda != "euro" && moneda != "euro blue") {
-    moneda = prompt("Elige una moneda entre (dolar,dolar blue,euro,euro blue)");
+const menu = () => {
+    ("1=dolar \n 2=dolar blue \n 3=euro \n 4=euro blue \n ingrese el numero correspondiente.")
 }
 
-// variables inicializada para darles valor luego
-let divisaSeleccionada;
-let divisa;
+let cortarFuncion = true;
 
-// operadores condicionales que asignan un valor a la variable inicializada anteriormente arriba segun la eleccion y muestran una alerta
-if (moneda == "dolar") {
-    alert("El valor del dolar oficial es: $" + dolar + " ars");
-    divisaSeleccionada = dolar;
-    divisa = " dolares"
-} else if (moneda == "dolar blue") {
-    alert("El valor del dolar blue es: $" + dolarBlue + " ars");
-    divisaSeleccionada = dolarBlue;
-    divisa = " dolares blue"
-} else if (moneda == "euro") {
-    alert("El valor del euro oficial es: $" + euro + " ars");
-    divisaSeleccionada = euro;
-    divisa = " euros"
-} else if (moneda == "euro blue") {
-    alert("El valor del euro blue es: $" + euro + " ars");
-    divisaSeleccionada = euroBlue;
-    divisa = " euros blue"
-}
+ function leerMoneda() {
 
-let ahorros = funcionAhorro();
+    while (cortarFuncion == true) {
+        moneda = prompt(menu());
+        if (moneda < 5 && moneda > 0) {
+            cortarFuncion == false;
+            return moneda
+        } else {
+            alert("el valor es incorrecto")
+            continue;
+        }
+    }
+} 
 
-if (ahorros == 404) {
-    alert("error numero invalido");
-} else {
-    alert("sus ahorros son " + ahorros / divisaSeleccionada + divisa);
-}
+
+divisaSeleccionada = leerMoneda();
 
 console.log(divisaSeleccionada);
 
+
+// Algoritmo condicional que asignan un valor a la variable inicializada anteriormente arriba segun la eleccion y muestran la alerta adecuada
+
+switch (Number(divisaSeleccionada)) {
+
+    case 1:
+
+        alert("Usted ha elegido 'dolar', el valor del dolar oficial es de : $" + dolar + " ars");
+        divisaSeleccionada = dolar;
+        divisa = " dolares"
+        break;
+
+    case 2:
+
+        alert("Usted ha elegido 'dolar blue', el valor del dolar blue es de : $" + dolarBlue + " ars");
+        divisaSeleccionada = dolarBlue;
+        divisa = " dolares blue"
+        break;
+
+    case 3:
+
+        alert("Usted ha elegido 'euro', el valor del euro es de : $" + euro + " ars");
+        divisaSeleccionada = euro;
+        divisa = "euros"
+        break;
+
+    case 4:
+
+        alert("usted ha elegido 'euro blue', el valor del euro blue es de : $" + euroBlue + " ars");
+        divisaSeleccionada = euroBlue;
+        divisa = "euros blue"
+        break;
+
+} 
+
+console.log(divisaSeleccionada);
+
+
+console.log(divisa);
+console.log(divisaSeleccionada);
+
+let ahorros = funcionAhorro();
+
+if (ahorros == numeroInvalido) {
+    alert("Error, el numero ingresado es invalido");
+} else {
+    alert("Sus ahorros son " + ahorros / divisaSeleccionada + " " + divisa);
+}
