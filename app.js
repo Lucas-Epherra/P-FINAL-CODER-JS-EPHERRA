f0421152bdde39f9510765b727b029994f91241b
Lucas Epherra <letoso13@gmail.com>
2022-10-23 03:00:56 -0300

    simulador de divisas v1

--- app.js
+++ app.js
@@ -2,61 +2,60 @@
 // a continuacion se realizara un simulador interactivo de un conversor de divisas ( euro, pesoArg, dolar y sus variantes blue) con lo visto hasta la clase 4.
 
 //variables ( monedas y valor )
+const peso = 1;
+const dolar = 200;
+const dolarBlue = 300;
+const euro = 210;
+const euroBlue = 310;
 
-let peso = 1;
-let dolar = 200;
-let dolarBlue = 300;
-let euro = 210;
-let euroBlue = 310;
+// funcion para verificar que en ahorro se tipee solo numeros
+function funcionAhorro() {
 
+    let ahorro = Number(prompt("Ingrese sus ahorros en pesos"));
 
-// funcion para elegir una divisa y ver su valor
-
-function elegirMoneda() {
-
-    let monedaElegida = prompt("Elija una moneda (dolar,dolar blue,euro,euro blue)");
-
-    if (monedaElegida == "dolar") {
-
-        monedaElegida = dolar;
-        alert("El valor del dolar oficial es : $" + monedaElegida + " ars");
-
-    } else if (monedaElegida == "dolar blue") {
-
-        let monedaElegida = prompt("Elija una moneda (dolar,dolar blue,euro,euro blue)");
-        monedaElegida = dolarBlue;
-        alert("El valor del dolar blue es $" + monedaElegida + " ars");
-
-    } else if (monedaElegida == "euro") {
-
-        let monedaElegida = prompt("Elija una moneda (dolar,dolar blue,euro,euro blue)");
-        monedaElegida = euro;
-        alert("El valor del euro es $" + monedaElegida + " ars");
-
-    } else if (monedaElegida == "euro blue") {
-
-        let monedaElegida = prompt("Elija una moneda (dolar,dolar blue,euro,euro blue)");
-        monedaElegida = euroBlue;
-        alert("El valor del euro blue es $" + monedaElegida + " ars");
-
-    } else if (monedaElegida != "euro","dolar","dolar blue","euro blue") {
-        monedaElegida = peso ;
-        prompt ("Elija una moneda valida(dolar,dolar blue,euro,euro blue)");
-    }
+    if (ahorro !== "" || ahorro !== null || ahorro !== NaN) {
+        return ahorro
+    } else { return 404 }
 }
 
-elegirMoneda();
+// prompt para que el usuario elija una moneda 
+let moneda = prompt("Elige una moneda entre (dolar,dolar blue,euro,euro blue)");
 
-console.log(monedaElegida);
-
-
-// funcion para saber a cuanto equivale una divisa de la otra x ejemplo (100 usd blue / 30.000 ars)
-/* 
-function montoIngresado() {
-
-    let = parseInt(prompt("Ingrese un monto"));
-
-
+// ciclo while para que solo se puedan elegir las 4 opciones
+while (moneda != "dolar" && moneda != "dolar blue" && moneda != "euro" && moneda != "euro blue") {
+    moneda = prompt("Elige una moneda entre (dolar,dolar blue,euro,euro blue)");
 }
 
-montoIngresado(); */
+// variables inicializada para darles valor luego
+let divisaSeleccionada;
+let divisa;
+
+// operadores condicionales que asignan un valor a la variable inicializada anteriormente arriba segun la eleccion y muestran una alerta
+if (moneda == "dolar") {
+    alert("El valor del dolar oficial es: $" + dolar + " ars");
+    divisaSeleccionada = dolar;
+    divisa = " dolares"
+} else if (moneda == "dolar blue") {
+    alert("El valor del dolar blue es: $" + dolarBlue + " ars");
+    divisaSeleccionada = dolarBlue;
+    divisa = " dolares blue"
+} else if (moneda == "euro") {
+    alert("El valor del euro oficial es: $" + euro + " ars");
+    divisaSeleccionada = euro;
+    divisa = " euros"
+} else if (moneda == "euro blue") {
+    alert("El valor del euro blue es: $" + euro + " ars");
+    divisaSeleccionada = euroBlue;
+    divisa = " euros blue"
+}
+
+let ahorros = funcionAhorro();
+
+if (ahorros == 404) {
+    alert("error numero invalido");
+} else {
+    alert("sus ahorros son " + ahorros / divisaSeleccionada + divisa);
+}
+
+console.log(divisaSeleccionada);
+
